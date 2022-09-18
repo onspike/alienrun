6b35a32902a06af452f38a9641139df3d27cc8f5
spike <spike@reducer-4.local>
2022-09-18 16:10:14 -0400

    postion test surface

--- game.py
+++ game.py
@@ -2,16 +2,19 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((800, 400))
+screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Run and Jump Simulator')
 clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((100,200))
+test_surface.fill('blue')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all our elements
-    # update everything
+        
+    screen.blit(test_surface,(200,100))
+
     pygame.display.update()
     clock.tick(60)
