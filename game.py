3deaee1534c0122b99a648dbeb83d68988e51dfc
spike <spike@reducer-4.local>
2022-09-25 15:02:58 -0400

    make snail move it

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import resource
 import pygame
 from sys import exit
 
@@ -6,15 +7,29 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Run and Jump Simulator')
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100,200))
-test_surface.fill('blue')
+test_font = pygame.font.Font('resources/font/Pixeltype.ttf', 50)
+
+sky_surface = pygame.image.load('resources/graphics/Sky.png')
+ground_surface = pygame.image.load('resources/graphics/ground.png')
+text_surface = test_font.render('Run and Jump Simulator', False, 'Black' )
+snail_surface = pygame.image.load('resources/graphics/snail/snail1.png')
+snail_x_pos = 800
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         
-    screen.blit(test_surface,(200,100))
-
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface, (210,50))
+    screen.blit(snail_surface, (snail_x_pos, 265))
     pygame.display.update()
+
+    snail_x_pos = snail_x_pos - 4
+
+    if snail_x_pos == -72:
+        snail_x_pos = 800
+
     clock.tick(60)
