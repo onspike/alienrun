0ad3947e85eb0c2ee6d0db96d29e515b14fed76c
spike <spike@reducer-4.local>
2022-10-15 16:29:45 -0400

    add collision and mouse

--- game.py
+++ game.py
@@ -16,18 +16,37 @@ player_surface = pygame.image.load('resources/graphics/Player/player_walk_1.png'
 snail_surface = pygame.image.load('resources/graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomleft = (800, 300))
 player_rect = player_surface.get_rect(bottomleft = (80, 300))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEMOTION:
+            mouse_pos = event.pos
+
     screen.blit(sky_surface, (0,0))        
     screen.blit(ground_surface, (0,300))
     screen.blit(text_surface, (210,50))
     screen.blit(snail_surface, snail_rect)
     screen.blit(player_surface, player_rect)
-    pygame.display.update()
+   
+    # if player_rect.colliderect(snail_rect):
+    #    print("collisson")
 
+    #mouse_pos = pygame.mouse.get_pos()
+    #is_mouse_pressed = pygame.mouse.get_pressed()
+
+    #if player_rect.collidepoint(mouse_pos) and is_mouse_pressed == (True, False, False):
+     #   print("ouch! you clicked on me!")
+
+    # mouse_pos = pygame.mouse.get_pos()
+
+    if player_rect.collidepoint(mouse_pos):
+       print("ouch! you hovered over me!")
+
+    pygame.display.update()
+    player_rect.left += 1
     snail_rect.left = snail_rect.left - 5
 
     if snail_rect.right < 0: 
