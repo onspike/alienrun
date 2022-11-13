5a52ef3cc0397b5214842c5298544c489a8058c2
spike <spike@reducer-4.local>
2022-11-13 17:23:35 -0500

    add jump and gravity

--- game.py
+++ game.py
@@ -15,9 +15,11 @@ test_font = pygame.font.Font('resources/font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('resources/graphics/Sky.png').convert()
 ground_surface = pygame.image.load('resources/graphics/ground.png').convert()
+ground_rect = ground_surface.get_rect(bottomleft = (0,470))
 
 player_surface = pygame.image.load('resources/graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surface.get_rect(bottomleft = (80, 300))
+player_gravity = 0
 
 snail_surface = pygame.image.load('resources/graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomleft = (800, 300))
@@ -39,22 +41,23 @@ while True:
         #     mouse_pos = event.pos
 
         if event.type == pygame.KEYDOWN:
-           if event.key == pygame.K_SPACE:
-            print('jump')
+           if event.key == pygame.K_SPACE: # and player_rect.bottom == ground_rect.top
+            player_gravity -= 12
 
         if event.type == pygame.KEYUP:
-            print('u')
+            print("key up")
+            player_gravity += 17
 
     screen.blit(sky_surface, (0,0))        
     screen.blit(ground_surface, (0,300))
     screen.blit(title_surface, title_rect)
     screen.blit(snail_surface, snail_rect)
+
     screen.blit(player_surface, player_rect)
+
     pygame.draw.rect(screen, (90,5,100), score_rect)
     pygame.draw.rect(screen, (89,50,10), score_rect, 10)
     screen.blit(score_surface, score_rect)
-    
-    
 
 
     #keys = pygame.key.get_pressed()
@@ -75,8 +78,17 @@ while True:
        print("ouch! you hovered over me!")
 
     pygame.display.update()
-    player_rect.left += 1
+    #player_rect.left += 1
     snail_rect.left = snail_rect.left - 5
+
+    player_rect.top += player_gravity
+
+    if player_rect.colliderect(ground_rect):
+        player_rect.bottom = ground_rect.top
+        player_gravity = 0
+
+    if player_rect.top < 110:
+        player_gravity += 4
 
     if snail_rect.right < 0: 
         snail_rect.left = 800
